fc4986fb89d5da31656103076d0f3a481e3145a9
focusotter <mtliendo@focusotter.com>
2023-08-03 14:15:34 -0500

    add prod url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
 	favicon: 'img/favicon.ico',
 
 	// Set the production url of your site here
-	url: 'https://your-docusaurus-test-site.com',
+	url: 'https://main.d1ev9d5gu41bgh.amplifyapp.com',
 	// Set the /<baseUrl>/ pathname under which your site is served
 	// For GitHub pages deployment, it is often '/<projectName>/'
 	baseUrl: '/',
